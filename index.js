de8219f0530c8de26a5f46d34b0961d83397056d
Camba <fercambafcm@gmail.com>
2024-07-23 13:28:33 -0600

    update

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res)=>{
+const PORT = process.env.PORT ?? 3000
 
+app.get('/', (req, res)=>{
+    res.send('SERVER: funcionando correctamente')
 })
 
+app.listen(PORT, ()=>{
+    console.log(`SERVER: servicios funcionando en el la siguiente direccion. \n ruta de servicio:          http://localhost:${PORT}`)
+})
