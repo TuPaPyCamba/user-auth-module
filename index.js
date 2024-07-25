607cf0b1f08e38b503e430f25283ae24a353bd6b
Camba <fercambafcm@gmail.com>
2024-07-25 14:34:31 -0600

    update

--- index.js
+++ index.js
@@ -1,30 +1,36 @@
 import express from "express";
 import { PORT } from "./config.js";
+import { UserRepository } from "./user-repository.js";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("SERVER: funcionando correctamente");
+    res.send("SERVER: operating correctly on the following port");
 });
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => { });
 
-})
+app.post("/register", (req, res) => {
+    const { username, password } = req.body;
 
-app.post('/register', (req, res) => {
+    try {
+        const id = UserRepository.create({ username, password });
+        res.send({ id });
+    } catch (error) {
+        console.log(
+            `SERVER: Error when trying to create a new User, with the following data \n Username: ${username} \n Password: ***** \n ESTATUS: ${error.message} \n`
+        );
+        res.status(400).send(error.message);
+    }
+});
 
-})
+app.post("/logout", (req, res) => { });
 
-app.post('/logout', (req, res) => {
-
-})
-
-app.get('/protected', (req, res) => {
-
-})
+app.get("/protected", (req, res) => { });
 
 app.listen(PORT, () => {
     console.log(
-        `SERVER: servicios funcionando en el la siguiente direccion. \n ruta de servicio:          http://localhost:${PORT}`
+        `SERVER: operating correctly on the following port \n \n service rute:  http://localhost:${PORT} \n`
     );
 });
--- user-repository.js
+++ user-repository.js
@@ -1,32 +1,39 @@
 import DBLocal from 'db-local'
-import Schema from 'db-local/lib/modules/schema'
+
+import crypto from 'node:crypto'
+import bcrypt from 'bcrypt'
+
+import { SALT_ROUNDS } from './config'
 
 const { Schema } = new DBLocal({ path: './db' })
 
 const User = Schema('User', {
     _id: { type: String, require: true },
-    username: { type: String, require: true },
-    password: { type: String, require: true }
+    username: { type: String, required: true },
+    password: { type: String, required: true }
 })
 export class UserRepository {
     static create({ username, password }) {
 
         if (typeof username !== 'string') throw new Error('username must be a string')
-        if (typeof username.length < 5) throw new Error('username must be at least 5 characters long')
+        if (username.length < 5) throw new Error('username must be at least 5 characters long')
 
         if (typeof password !== 'string') throw new Error('password must be a string')
-        if (typeof password.length < 8) throw new Error('password must be at least 8 characters long')
+        if (password.length < 8) throw new Error('password must be at least 8 characters long')
         
         const user = User.findOne({ username })
         if (user) throw new Error('username already exists')
 
         const id = crypto.randomUUID()
+        const hasedPassword = bcrypt.hashSync(password, SALT_ROUNDS)
 
         User.create({
             _id: id,
             username,
-            password
+            password, hasedPassword
         }).save()
+
+        console.log(`SERVER: New user created \n ID: ${id} \n Username: ${username} \n Password: ${password} \n`)
 
         return id
     }
